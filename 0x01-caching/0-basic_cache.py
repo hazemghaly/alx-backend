44083a0a24227ac12990caa4a07b2dffb3ce6994
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-04 01:39:53 +0200

    Add files via upload

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,14 +11,20 @@ class BasicCache(BaseCaching):
       - where your data are stored (in a dictionary)
     """
     def __init__(self):
+        """ Initiliaze
+        """
         super().__init__()
 
     def put(self, key, item):
+        """ method Put
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """ method get
+        """
         if key is None or key not in self.cache_data:
             return None
         return (self.cache_data[key])
