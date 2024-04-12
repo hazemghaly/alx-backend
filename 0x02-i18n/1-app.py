f1805ddca2503dd3bf896ad87c08f1d81d6bbf17
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-12 17:30:02 +0200

    Update 1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""basic flask app
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config:
+    """Configurations for the Flask app."""
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def home():
+    """home function define yor title and header"""
+    return render_template("0-index.html", title="“Welcome to Holberton")
