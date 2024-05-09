a0d0bc5f4a613a3a38f44843b979fc6e16b8809f
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-09 04:00:25 +0300

    Add files via upload

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+import { createClient } from 'redis';
+
+const client = createClient()
+
+  client.on('error', ERROR_MESSAGE  => console.log(`Redis client not connected to the server: ${ERROR_MESSAGE }`))
+  client.on('connect', () => {
+    console.log('Redis client connected to the server')})
+
+function setNewSchool(schoolName,value){
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error('Error setting school value:', err);
+    } else {
+      console.log(reply);
+    }
+  });}
+
+function displaySchoolValue(schoolName){
+  client.get(schoolName, (err, value) => {
+    if (err) {
+      console.error('Error getting school value:', err);
+    } else {
+      console.log(value);
+    }
+  });}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
