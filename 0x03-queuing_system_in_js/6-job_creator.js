6b137e604ce4f54e2c99fdcb7d6fd29821894298
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-09 05:29:26 +0300

    Add files via upload

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+import kue from 'kue';
+
+const queue = kue.createQueue({ name: 'push_notification_code' });
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '01239',
+  message: 'Acc',
+});
+
+job.on('enqueue', () => {
+  console.log('Notification job created:', job.id);
+  }).on('failed attempt', () => {
+  	console.log('Notification job failed');
+	}).on('complete', () => {
+		console.log('Notification job completed');
+	});
+job.save();
+
+
+
+
+
